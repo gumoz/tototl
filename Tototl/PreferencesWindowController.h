da78fa04b219f43b6446dc8384832246e3e5762e
gm@ixaya.com <gm@ixaya.com@8bc18d56-bb6b-11dd-9fdb-df1701488ebc>
2009-03-22 23:36:33 +0000

    Updated MGTwitterEngine To revision 37

---
+++ Tototl/PreferencesWindowController.h
@@ -0,0 +1,14 @@
+//
+//  PreferencesWindowController.h
+//  Tototl
+//
+//  Created by Gustavo Moya Ortiz on 3/22/09.
+//  Copyright 2009 Ixaya. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PreferencesWindowController : NSWindowController {
+
+}
+@end
