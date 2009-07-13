fd65d5909d6d89dda04a3da6c41d0d0e2d1e21b2
gm@ixaya.com <gm@ixaya.com@8bc18d56-bb6b-11dd-9fdb-df1701488ebc>
2009-07-13 18:15:19 +0000

    Tagged v1.4

---
+++ PreferencesWindowController.h
@@ -0,0 +1,33 @@
+//
+//  PreferencesWindowController.h
+//  Tototl
+//
+//  Created by Gustavo Moya Ortiz on 3/15/08.
+//  Copyright 2008 Ixaya. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "MGTwitterEngine.h"
+
+@interface PreferencesWindowController : NSWindowController {
+	IBOutlet id preferences;
+	IBOutlet id profileView;
+	IBOutlet id twitterView;
+	IBOutlet id tototlView;
+	IBOutlet id interfaceView;
+	IBOutlet id updatesView;
+	IBOutlet id aboutView;
+    int currentViewTag;
+	MGTwitterEngine *twitterEngine;
+	IBOutlet id segmentedControl;
+	NSArray *deliveryMethods;
+}
+@property (retain, readwrite) MGTwitterEngine *twitterEngine;
+-(NSRect)newFrameForNewContentView:(NSView *)view;
+-(NSView *)viewForTag:(int)tag;
+-(IBAction)switchView:(id)sender;
+-(IBAction)checkForUpdates:(id)sender;
+-(IBAction)openIUseThis:(id)sender;
+-(IBAction)openDonate:(id)sender;
+-(IBAction)setProfile:(id)sender;
+@end
