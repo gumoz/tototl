639c111d9f3ec456ceb5516179a86dac76a2cee8
gm@ixaya.com <gm@ixaya.com@8bc18d56-bb6b-11dd-9fdb-df1701488ebc>
2009-07-13 15:59:26 +0000

    Tagged v1.6

---
+++ tags/v1.6/IXTwitterMessage.h
@@ -0,0 +1,35 @@
+//
+//  IXTwitterMessage.h
+//  Ixaya Twitter
+//
+//  Created by Gustavo Moya Ortiz on 20/11/08.
+//  Copyright 2008 Ixaya. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface IXTwitterMessage : NSObject {
+	
+	int kind;
+	NSImage *picture;
+	NSData *picture_data;
+	NSString *name;
+	NSString *message;
+	NSAttributedString *attributedMessage;
+	NSString *twitterId;
+	NSDate *date;
+	NSString *screenName;
+}
+@property int kind;
+@property (copy, readwrite) NSImage *picture;
+@property (copy, readwrite)	NSData *picture_data;
+@property (copy, readwrite) NSString *name;
+@property (copy, readwrite) NSString *message;
+@property (copy, readwrite) NSAttributedString *attributedMessage;
+@property (copy, readwrite) NSString *twitterId;
+@property (copy, readwrite) NSDate *date;
+@property (copy, readwrite) NSString *screenName;
+-(void)setPictureUsingUrl:(NSString *)url;
+
+@end
